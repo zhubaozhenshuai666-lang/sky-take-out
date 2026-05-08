299509ccfe6928499737fdc634703753ae856aa5
Zewbbb <2423766353@qq.com>
2026-05-08 20:02:39 +0800

    新增员工业务代码

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -14,12 +14,15 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 配置类，注册web层相关组件
  */
 @Configuration
 @Slf4j
+@EnableSwagger2
+
 public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
     @Autowired
